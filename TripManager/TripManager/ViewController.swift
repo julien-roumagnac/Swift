304ac9575a58c5b9f582c8dc99482e65a356d8f3
Julien Roumagnac <julienroumagnac@iMac.isim.intra>
2019-03-21 19:18:11 +0100

    initial commit

---
+++ TripManager/TripManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripManager
+//
+//  Created by Julien Roumagnac on 21/03/2019.
+//  Copyright © 2019 Julien Roumagnac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
